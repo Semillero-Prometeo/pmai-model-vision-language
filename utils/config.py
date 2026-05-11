24d8a669c85795831e7c05ef2c92e631ebd1a7a0
anpalop <anpalop@ibm.com>
2026-05-10 21:27:03 -0500

    Refactor code structure for improved readability and maintainability

--- utils/config.py
+++ utils/config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 def get_GPT_token():
     """
-    Here we will load the APIKEY since the .env file
+
     """
 
     load_dotenv()
-    return os.getenv("")
+    return os.getenv("OPENAI_API_KEY")
