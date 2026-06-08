e04d32f0b72e69b92d535a9e9fb58baa2d3c80e7
anpalop <anpalop@ibm.com>
2026-06-08 00:09:56 -0500

    Add initial __init__.py files for multiple directories and update OpenAI model configuration

--- utils/config.py
+++ utils/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 OLLAMA_URL = os.getenv("OLLAMA_URL", "http://localhost:11434")
 OLLAMA_MODEL = os.getenv("OLLAMA_MODEL", "qwen2.5:0.5b")
-OPEN_AI_MODEL = os.getenv("OPEN_AI_MODEL", "gpt-3.5-turbo")
+OPEN_AI_MODEL = os.getenv("OPEN_AI_MODEL", "gpt")
 
 
 
